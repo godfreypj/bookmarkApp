aef543b924e5a4eeb2b349794fbaa70040bde857
Philip Godfrey <60916447+godfreypj@users.noreply.github.com>
2020-04-01 14:41:56 +1000

    Add files via upload

---
+++ mainMenu.js
@@ -0,0 +1,38 @@
+module.exports = 
+[
+    {
+      label: "Electron",
+      submenu: [
+        { label: "Item 1"},
+        { label: "Item 2", submenu: [ { label: "Sub Item 1"} ] },
+        { label: "Item 3"},
+      ]
+    },
+    { label: "Edit",
+      submenu: [
+        { role: "undo" },
+        { role: "redo" },
+        { role: "copy" },
+        { role: "paste" },
+      ]
+    },
+    {
+      label: "Actions",
+      submenu: [
+        { 
+          label: "DevTools",
+          role: "toggleDevTools",
+        },
+        { label: "Action Item 2", submenu: [ { label: "Toggle Full Screen",
+                                               role: "toggleFullScreen",
+                                              }
+                                            ]}, 
+        { 
+          label: "Greet",
+          click: function(){
+            console.log("Hello from the Menu!");            
+            accelerator: "CommandOrControl+G"
+          }},
+      ]
+    }
+  ]
